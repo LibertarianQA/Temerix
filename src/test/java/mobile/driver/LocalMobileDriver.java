b770d94c4e5a75b72d78f6dceac075969c6f4d40
Libertarian QA <libertarianqa@gmail.com>
2025-04-06 16:01:44 +0300

    v

--- src/test/java/mobile/driver/LocalMobileDriver.java
+++ src/test/java/mobile/driver/LocalMobileDriver.java
@@ -14,22 +14,22 @@ import static io.appium.java_client.remote.MobilePlatform.ANDROID;
 
 public class LocalMobileDriver implements WebDriverProvider {
 
-    public static URL getAppiumServerUrl() {
-        try {
-            return new URL(System.getProperty("AppiumURL", "http://localhost:4723"));
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-//public static URL getAppiumServerUrl() {
-//    String url = System.getProperty("JenkinsAppium", "http://localhost:4723/");
-//    System.out.println(">>> Using Appium server URL: " + url);
-//    try {
-//        return new URL(url);
-//    } catch (MalformedURLException e) {
-//        throw new RuntimeException(e);
+//    public static URL getAppiumServerUrl() {
+//        try {
+//            return new URL(System.getProperty("AppiumURL", "http://localhost:4723"));
+//        } catch (MalformedURLException e) {
+//            throw new RuntimeException(e);
+//        }
 //    }
-//}
+public static URL getAppiumServerUrl() {
+    String url = System.getProperty("JenkinsAppium", "http://localhost:4723/");
+    System.out.println(">>> Using Appium server URL: " + url);
+    try {
+        return new URL(url);
+    } catch (MalformedURLException e) {
+        throw new RuntimeException(e);
+    }
+}
 
 
     @Override
